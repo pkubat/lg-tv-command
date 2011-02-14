1ddcdea94b8c223543f7f34dc6c2bb8a80e1659b
darkfader <darkfader@c08161a5-4b4e-2339-52f7-e9331a227556>
2011-02-14 21:19:16 +0000

    Added more commands, better response check, prefix current value from list when reading.

--- trunk/lg-tv-command.c
+++ trunk/lg-tv-command.c
@@ -14,6 +14,10 @@
 #include <sys/time.h>
 #include <stdbool.h>
 
+#define DEVICE			"/dev/tty.usbserial"
+
+#define READ_STATUS		0xFF
+
 /*
  * list macros
  */
@@ -52,7 +56,7 @@
 
 typedef struct
 {
-	unsigned char i;
+	unsigned char value;
 	const char *name;
 } SubDef;
 
@@ -77,14 +81,16 @@ typedef struct
 	const char *name;
 	char cmd1;
 	char cmd2;
+	unsigned char min;
+	unsigned char max;
 	SubDef *list;
 	int listlen;
 } CmdDef;
 
-#define NAME_CMD1_CMD2_LIST_CODE(m,c1,c2,l)
-#define NAME_CMD1_CMD2_LIST_ENUM(m,c1,c2,l)			m##_INDEX,
-#define NAME_CMD1_CMD2_LIST_STRUCT(m,c1,c2,l)		{ #m, c1, c2, LIST(l), LENGTH(l) },
-#define NAME_CMD1_CMD2_LIST_STYPE					CmdDef
+#define NAME_CMD_LIST_CODE(m,c1,c2,min,max,l)
+#define NAME_CMD_LIST_ENUM(m,c1,c2,min,max,l)		m##_INDEX,
+#define NAME_CMD_LIST_STRUCT(m,c1,c2,min,max,l)		{ #m, c1, c2, min, max, LIST(l), LENGTH(l) },
+#define NAME_CMD_LIST_STYPE							CmdDef
 
 
 
@@ -128,6 +134,23 @@ typedef struct
 	_(Original,			6)\
 	_(_14_9,			7)\
 	_(JustScan,			9)\
+	_(FullWide,			0x0B)\
+	_(CinemaZoom1,		0x10)\
+	_(CinemaZoom2,		0x11)\
+	_(CinemaZoom3,		0x12)\
+	_(CinemaZoom4,		0x13)\
+	_(CinemaZoom5,		0x14)\
+	_(CinemaZoom6,		0x15)\
+	_(CinemaZoom7,		0x16)\
+	_(CinemaZoom8,		0x17)\
+	_(CinemaZoom9,		0x18)\
+	_(CinemaZoom10,		0x19)\
+	_(CinemaZoom11,		0x1A)\
+	_(CinemaZoom12,		0x1B)\
+	_(CinemaZoom13,		0x1C)\
+	_(CinemaZoom14,		0x1D)\
+	_(CinemaZoom15,		0x1E)\
+	_(CinemaZoom16,		0x1F)\
 
 #define ISM_METHOD_TYPE(_)			NIT(_,NAME_INDEX)
 #define ISM_METHOD_LIST(_)\
@@ -145,6 +168,17 @@ typedef struct
 
 #define REMOTE_TYPE(_)				NIT(_,NAME_INDEX)
 #define REMOTE_LIST(_)\
+	_(TV_RADIO,		0xF0)\
+	_(RED_KEY,		0x72)\
+	_(GREEN_KEY,	0x71)\
+	_(YELLOW_KEY,	0x63)\
+	_(BLUE_KEY,		0x61)\
+	_(TEXT,			0x20)\
+	_(T_OPTION,		0x21)\
+	_(FREEZE,		0x65)\
+	_(AV_MODE,		0x30)\
+	_(INFO,			0xAA)\
+	_(ENERGY,		0x95)\
 	_(TV,			0x0F)\
 	_(INPUT,		0x0B)\
 	_(POWER,		0x08)\
@@ -162,6 +196,7 @@ typedef struct
 	_(_9,			0x19)\
 	_(DASH,			0x4C)\
 	_(BACK,			0x1A)\
+	_(Q_VIEW,		0x1A)\
 	_(MUTE,			0x09)\
 	_(VOL_UP,		0x02)\
 	_(VOL_DOWN,		0x03)\
@@ -169,15 +204,17 @@ typedef struct
 	_(CHAN_DOWN,	0x01)\
 	_(FAV,			0x1E)\
 	_(SIMPLINK,		0x7E)\
-	_(CC,			0x39)\
+	_(SUBTITLE,		0x39)\
 	_(UP,			0x40)\
 	_(DOWN,			0x41)\
 	_(LEFT,			0x07)\
 	_(RIGHT,		0x06)\
 	_(ENTER,		0x44)\
 	_(MENU,			0x43)\
+	_(GUIDE,		0xAB)\
 	_(SOUND,		0x52)\
 	_(PICTURE,		0x4D)\
+	_(RETURN_EXIT,	0x28)\
 	_(EXIT,			0x5B)\
 	_(SAP,			0x0A)\
 	_(ADJUST,		0xCB)\
@@ -210,31 +247,33 @@ DEF_LIST(LISTS)
 
 #define _LIST			NULL
 
-#define CMD_TYPE(_)			NIT(_,NAME_CMD1_CMD2_LIST)
+#define CMD_TYPE(_)			NIT(_,NAME_CMD_LIST)
 #define CMD_LIST(_)\
-	_(Power,			'k','a',)\
-	/*_(InputSelect,		'k','b',INPUT_SELECT1)*/\
-	_(AspectRatio,		'k','c',ASPECT_RATIO)\
-	_(ScreenMute,		'k','d',)\
-	_(VolumeMute,		'k','e',)\
-	_(VolumeControl,	'k','f',)\
-	_(Contrast,			'k','g',)\
-	_(Brightness,		'k','h',)\
-	_(Color,			'k','i',)\
-	_(Tint,				'k','j',)\
-	_(Sharpness,		'k','k',)\
-	_(OSDSelect,		'k','l',)\
-	_(RemoteLock,		'k','m',)\
-	_(Treble,			'k','r',)\
-	_(Bass,				'k','s',)\
-	_(Balance,			'k','t',)\
-	_(ColorTemperature,	'k','u',COLOR_TEMPERATURE)\
-	_(ISMMethod,		'j','p',ISM_METHOD)\
-	_(LowPower,			'j','q',)\
-	_(ChannelTuning,	'm','a',)/*TODO: extra data bytes*/\
-	_(ChannelAddDel,	'm','b',)\
-	_(Key,				'm','c',REMOTE)\
-	_(InputSelect2,		'x','b',INPUT_SELECT2)\
+	_(Power,			'k','a',0,1,)\
+	/*_(InputSelect1,		'k','b',0,0,INPUT_SELECT1)*/\
+	_(AspectRatio,		'k','c',0,0,ASPECT_RATIO)\
+	_(ScreenMute,		'k','d',0,1,)\
+	_(VolumeMute,		'k','e',0,1,)\
+	_(VolumeControl,	'k','f',0,100,)\
+	_(Contrast,			'k','g',0,100,)\
+	_(Brightness,		'k','h',0,100,)\
+	_(Color,			'k','i',0,100,)\
+	_(Tint,				'k','j',0,100,)\
+	_(Sharpness,		'k','k',0,100,)\
+	_(OSDSelect,		'k','l',0,1,)\
+	_(RemoteLock,		'k','m',0,1,)\
+	_(Treble,			'k','r',0,100,)\
+	_(Bass,				'k','s',0,100,)\
+	_(Balance,			'k','t',0,100,)\
+	_(ColorTemperature,	'k','u',0,0,COLOR_TEMPERATURE)\
+	_(ISMMethod,		'j','p',0,0,ISM_METHOD)\
+	_(EnergySaving,		'j','q',0,4,)\
+	_(AutoConfiguration,'j','u',1,1,)\
+	_(ChannelTuning,	'm','a',0,0,)/*TODO: extra data bytes*/\
+	_(ChannelAddDel,	'm','b',0,1,)\
+	_(Key,				'm','c',0,0,REMOTE)\
+	_(ControlBackLight,	'm','g',0,100,)\
+	_(InputSelect2,		'x','b',0,0,INPUT_SELECT2)\
 
 DEF_LIST(CMD)
 
@@ -246,37 +285,96 @@ DEF_LIST(CMD)
 #define TIMEOUT		1.0
 const int set_id = 1;
 
-int fd;		// filedescriptor for serial port
+int fd = -1;		// filedescriptor for serial port
 
 struct termios tios_stdin;
 
-void ResetTerminal()
+/*
+ * Clean up at exit
+ */
+void AtExit()
 {
+	// Close serial port
+	if (fd != -1) close(fd);
+
+	// Restore stdin on exit
     tcsetattr(fileno(stdin), TCSANOW, &tios_stdin);
 }
 
+/*
+ * Set unbuffered stdin/stdout
+ */
+void InitStdio()
+{
+    tcgetattr(fileno(stdin), &tios_stdin);
+	atexit(AtExit);
+
+    struct termios ts;
+    tcgetattr(fileno(stdin), &ts);
+    ts.c_lflag &=~ (ICANON | ECHO /*| ISIG*/);
+    ts.c_cc[VMIN] = 1;      // non blocking
+    ts.c_cc[VTIME] = 1;     // no timer
+    tcsetattr(fileno(stdin), TCSANOW, &ts);
+
+    setbuf(stdout, 0);
+}
 
 /*
- * value 0xFF = read status
+ * Open serial port
+ */
+void InitSerial()
+{
+	if (fd != -1) return;	// already opened
+	
+	char *device = DEVICE;
+    fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if (fd < 0) { fprintf(stderr, "Cannot open '%s'\n", device); exit(1); }
+    tcflush(fd, TCIOFLUSH);
+
+	struct termios tios;
+    memset(&tios, 0, sizeof(tios));
+    tios.c_ispeed = tios.c_ospeed = B9600;
+    cfsetispeed(&tios, B9600);
+    cfsetospeed(&tios, B9600);
+    tios.c_cflag = CS8 | CLOCAL | CREAD;
+    tios.c_iflag = IGNPAR | IGNBRK | IXANY;// | INPCK;
+    tios.c_lflag = ISIG;
+    tios.c_cc[VMIN] = 1;        // non blocking
+    tios.c_cc[VTIME] = 0;       // no timer
+    tcsetattr(fd, TCSANOW, &tios);
+
+	tcflush(fd, TCIOFLUSH);
+	write(fd, "\r", 1);		// reset, wake up, whatever
+}
+
+/*
+ * Send command to LG TV
  *
- * -256 = timeout
+ * value can be READ_STATUS
+ *
+ * return:
+ * -256 = timeout/unknown response
  * +n = OK
  * -n = NG
  *   -1 illegal code
  *   -2 not supported function
  *   -3 wait more time
+ *   hmm.. really?
  */
 int SendCommand(char cmd1, char cmd2, unsigned char value)
 {
+	InitSerial();
+	
 	char cmd[20];
-	sprintf(cmd, "%c%c %02x %02x\r", cmd1, cmd2, set_id, (int)value);
-//	printf("%s\n", cmd);
- 	write(fd, cmd, strlen(cmd));
+	int len = sprintf(cmd, "%c%c %02x %02x\r", cmd1, cmd2, set_id, (int)value);
+	//printf("%s\n", cmd);
+	write(fd, cmd, len);
+	tcdrain(fd);
 	memset(cmd, 0, sizeof(cmd));
 	char *p = cmd;
 
-    struct timeval tv1; gettimeofday(&tv1, NULL);
-    double t1 = tv1.tv_sec + (tv1.tv_usec / 1000000.0);
+	struct timeval tv1; gettimeofday(&tv1, NULL);
+	double t1 = tv1.tv_sec + (tv1.tv_usec / 1000000.0);
 	
 	while (true)
 	{
@@ -284,34 +382,51 @@ int SendCommand(char cmd1, char cmd2, unsigned char value)
         double t2 = tv2.tv_sec + (tv2.tv_usec / 1000000.0);
         if (t2 > t1 + TIMEOUT)
 		{
-			printf("timeout\n");
+			fprintf(stderr, "Response timeout: %s\n", cmd);
 			return -256;
 		}
 
+		// try reading a character
         if (read(fd, p, 1) >= 1) if (p - cmd < sizeof(cmd))
         {
+			if (*p == 'x')		// there's no real newline in response. hopefully cmd2 is never a 'x'
+			{
+				char cmd2r;
+				int id;
+				char ok_ng[2];
+				int data;
+				char x;
+				if (sscanf(cmd, " %c %02x %2c%02x%c", &cmd2r, &id, ok_ng, &data, &x) == 5)
+				{
+					if ((x == 'x') && (id == set_id) && (cmd2r == cmd2))
+					{
+						if (ok_ng[0] == 'O' && ok_ng[1] == 'K')
+						{
+							if ((value == READ_STATUS) || (value == data))
+							{
+	//							printf("OK %02X\n", value);
+								return data;
+							}
+						}
+						else if (ok_ng[0] == 'N' && ok_ng[1] == 'G')
+						{
+	//						fprintf(stderr, "NG %02X\n", data);
+							return -data;
+						}
+					}
+					fprintf(stderr, "Unknown response: %s\n", cmd);					
+					return -256;
+				}
+			}
 			p++;
-			char cmd2r;
-			int id;
-			int data;
-			if ((sscanf(cmd, "%c %02x OK%02xx", &cmd2r, &id, &data) == 3) && (id == set_id) && (cmd2r == cmd2))
-			{
-				//printf("OK %02X\n", data);
-				return data;
-			}
-			if ((sscanf(cmd, "%c %02x NG%02xx", &cmd2r, &id, &data) == 3) && (id == set_id) && (cmd2r == cmd2))
-			{
-				//printf("NG %02X\n", data);
-				return -data;
-			}
 		}
 
-		usleep(1*1000);
+		usleep(1000);
 	}
 }
 
 /*
- *
+ * Make string lowercase etc.
  */
 void strfix(char *x)
 {
@@ -327,122 +442,119 @@ void strfix(char *x)
  */
 int main(int argc, char *argv[])
 {
+	InitStdio();
+	
+	char *argv_cmd = (argc >= 2) ? argv[1] : NULL;
+	char *argv_value = (argc >= 3) ? argv[2] : NULL;
+	
 	char cmd1, cmd2;
 	int value = -1;
 
-	if (argc < 2)
+	/*
+	 * Show usage
+	 */
+	if ((argv_cmd == NULL) && (argv_value == NULL))
 	{
-		printf("Syntax: lg [cmd] [param]\n\n");
+		printf("Usage: lg-tv-command [command] [value]\n\n");
 		LIST_STYPE(CMD) *commands = LIST(CMD);
 		for (int l=0; l<LENGTH(CMD); l++)
 		{
-			printf("%s\n", commands[l].name);
+			printf("  %s\n", commands[l].name);
 		}
-		return 0;
+		return 1;
 	}
 	
-	char *a = strdup(argv[1]);
-	strfix(a);
+	char *argv_cmd2 = strdup(argv_cmd);
+	strfix(argv_cmd2);
 
 	LIST_STYPE(CMD) *commands = LIST(CMD);
 	for (int l=0; l<LENGTH(CMD); l++)
 	{
-		char *b = strdup(commands[l].name);
-		strfix(b);
-		if (strstr(b, a) != NULL)
+		char *name2 = strdup(commands[l].name);
+		strfix(name2);
+		if (strstr(name2, argv_cmd2) != NULL)
 		{
-			printf("%s:\n\n", commands[l].name);
 			cmd1 = commands[l].cmd1;
 			cmd2 = commands[l].cmd2;
 
-			if (argc < 3)
+			/*
+			 * Show current value and possible values
+			 */
+			if (argv_value == NULL)
 			{
+				if (argv_value == NULL) printf("%s:\n\n", commands[l].name);
+				int r = SendCommand(cmd1, cmd2, READ_STATUS);
+				if (r >= 0)
+				{
+					printf("Current value: %u (0x%02X).", r, r);
+					if ((commands[l].min != 0) || (commands[l].max != 0))
+						printf(" Range: %u-%u.", commands[l].min, commands[l].max);
+					printf("\n\n");
+				}
+				else
+				{
+					printf("Response error %d.\n\n", r);
+				}
+				
 				SubDef *list = commands[l].list;
 				if (list != NULL)
 				{
 					for (int s=0; s<commands[l].listlen; s++)
 					{
+						if (r == list[s].value)
+							printf("=>");
+						else
+							printf("  ");
 						printf("%s\n", list[s].name);
 					}
-				}
-				else
-				{
-					printf("numeric value\n");
 				}
 				return 0;
 			}
 
-			char *a2 = strdup(argv[2]);
-			strfix(a2);
-
+			/*
+			 * Determine value by matching enum
+			 */
 			SubDef *list = commands[l].list;
 			if (list != NULL)
 			{
+				char *argv_value2 = strdup(argv_value);
+				strfix(argv_value2);
 				for (int s=0; s<commands[l].listlen; s++)
 				{
 					char *c = strdup(list[s].name);
 					strfix(c);
-					if (strstr(c, a2) != NULL)
+					if (strstr(c, argv_value2) != NULL)
 					{
 						printf("%s\n", list[s].name);
-						value = list[s].i;
+						value = list[s].value;
 						break;
 					}
 					free(c);
 				}
+				free(argv_value2);
 			}
-			else
+			if (value < 0)		// no enum or it did not match
 			{
-				value = strtoul(argv[2],0,0);
+				value = strtoul(argv_value,0,0);
 			}
 
 			break;
 		}
-		free(b);
+		free(name2);
 	}
 
 	if (value < 0)
 	{
-		printf("Invalid value\n");
+		printf("Invalid value.\n");
 		return -1;
 	}
 
-    tcgetattr(fileno(stdin), &tios_stdin);
-	atexit(ResetTerminal);
-
-    struct termios ts;
-    tcgetattr(fileno(stdin), &ts);
-    ts.c_lflag &=~ (ICANON | ECHO /*| ISIG*/);
-    ts.c_cc[VMIN] = 1;      // non blocking
-    ts.c_cc[VTIME] = 1;     // no timer
-    tcsetattr(fileno(stdin), TCSANOW, &ts);
-    setbuf(stdout, 0);
-
-	char *device = "/dev/tty.usbserial";	// argv[1]
-    fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if (fd < 0) { fprintf(stderr, "Cannot open '%s'\n", device); return 1; }
-    tcflush(fd, TCIOFLUSH);
-
-	struct termios tios;
-    memset(&tios, 0, sizeof(tios));
-    tios.c_ispeed = tios.c_ospeed = B9600;
-    cfsetispeed(&tios, B9600);
-    cfsetospeed(&tios, B9600);
-    tios.c_cflag = CS8 | CLOCAL | CREAD;
-    tios.c_iflag = IGNPAR | IGNBRK | IXANY;// | INPCK;
-    tios.c_lflag = ISIG;
-    tios.c_cc[VMIN] = 1;        // non blocking
-    tios.c_cc[VTIME] = 0;       // no timer
-    tcsetattr(fd, TCSANOW, &tios);
-
- 	write(fd, "\r\r\r", 3);		// blah blah blah
 	int r = SendCommand(cmd1, cmd2, value);
 	if (r >= 0)
 	{
 		//printf("OK!\n");
+		return 0;
 	}
 
-	close(fd);
-	return r;
+	return -r;		// make positive again
 }
-
